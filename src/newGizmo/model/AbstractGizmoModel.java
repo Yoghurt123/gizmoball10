72bd32e159ffb6914aa64ffe0125fba3103ae291
mikiones <mikiones@gmail.com>
2012-03-21 18:22:57 +0000

    Added Linkinkg gizmos posibility and 2 helping methods to activate/deactivate all linked gizmos at once

--- src/newGizmo/model/AbstractGizmoModel.java
+++ src/newGizmo/model/AbstractGizmoModel.java
@@ -1,5 +1,7 @@
 package newGizmo.model;
 
+import java.util.LinkedList;
+
 import newGizmo.Drawable;
 import newGizmo.GizmoDriver;
 import newGizmo.SavleLoadable;
@@ -32,7 +34,7 @@ public abstract class AbstractGizmoModel implements Drawable, Colisoinable,
 			/**
 			 * calculate new ball movement after reflection
 			 */
-			onColisionTime(board.getBall(),reflecFrom);
+			onColisionTime(board.getBall(), reflecFrom);
 			/**
 			 * activate hited gizmo
 			 */
@@ -45,9 +47,56 @@ public abstract class AbstractGizmoModel implements Drawable, Colisoinable,
 	protected int y = 0;
 	protected String name;
 
+	protected LinkedList<AbstractGizmoModel> linkedGizmos = null;
+
 	public AbstractGizmoModel(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	/**
+	 * activate all gizmos linked to this one
+	 */
+	protected void activateLinkedGizmos() {
+		if (linkedGizmos != null)
+			for (AbstractGizmoModel gizmo : linkedGizmos) {
+				gizmo.onActivationEvent();
+			}
+	}
+
+	/**
+	 * deactivate all gizmos linked to this one
+	 */
+	protected void deactivateLinkedGizmos() {
+		if (linkedGizmos != null)
+			;
+		for (AbstractGizmoModel gizmo : linkedGizmos) {
+			gizmo.onDeactivationEvent();
+		}
+	}
+
+	/**
+	 * link gizmo to this one
+	 * 
+	 * @param gizmo
+	 */
+	public void linkGizmo(AbstractGizmoModel gizmo) {
+		if (linkedGizmos == null)
+			linkedGizmos = new LinkedList<AbstractGizmoModel>();
+		linkedGizmos.add(gizmo);
+	}
+
+	/**
+	 * unlink gizmo from this one
+	 * 
+	 * @param gizmo
+	 */
+	public void unlinkGizmo(AbstractGizmoModel gizmo) {
+		if (linkedGizmos != null) {
+			linkedGizmos.remove(gizmo);
+			if (linkedGizmos.isEmpty())
+				linkedGizmos = null;
+		}
 	}
 
 	public abstract void onHitEvent();
