022be9e0d0dcf4ce6d3f35945e2d7ad500810638
mikiones <mikiones@gmail.com>
2012-03-26 19:26:36 +0100

    absorber to absorb

--- src/newGizmo/model/Absorber.java
+++ src/newGizmo/model/Absorber.java
@@ -3,7 +3,10 @@ package newGizmo.model;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import newGizmo.GizmoDriver;
 import newGizmo.GizmoSettings;
+import newGizmo.Utils;
+import newGizmo.model.AbstractGizmoModel.onColisionTimeTask;
 
 import physics.Angle;
 import physics.Geometry;
@@ -13,8 +16,9 @@ import physics.Vect;
 public class Absorber extends AbstractGizmoModel {
 
 	int length = GizmoSettings.getInstance().getGizmoL();
-	
+
 	LineSegment abs;
+
 	public Absorber(int x, int y) {
 		super(x, y);
 		SetBoundary();
@@ -25,45 +29,54 @@ public class Absorber extends AbstractGizmoModel {
 	public Graphics paint(Graphics g) {
 		g.setColor(Color.BLACK);
 
-		g.fillRect(0, (int)y, 600, 20);
- 
+		g.fillRect(0, (int) y, 600, 20);
+
 		return g;
 	}
 
 	@Override
 	public void update(double dtime) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	public void SetBoundary(){
-		abs = new LineSegment(0,y,20*length,y);
+
+	public void SetBoundary() {
+		abs = new LineSegment(0, y, 20 * length, y);
 	}
-	
+
 	public LineSegment getRect() {
-	
-	return abs;
-}
+
+		return abs;
+	}
 
 	@Override
 	public double timeToColision(GizmoBall ball) {
-		double absorbtemp;
-//		Geometry.t
-		absorbtemp = Geometry.timeUntilWallCollision(abs, ball.getShape(), ball.getVolecity());
-		
-		if(absorbtemp<GizmoSettings.getInstance().getBallMovementUpdateDtime()){
-			GizmoBall b= new GizmoBall(200, 200, new Vect(20,-104.5));
-			GizmoBoard.getInstance().setBall(b);		
-			b.startBallMovement();
+		double tempTime;
 
-		}
-		return absorbtemp;	
+		tempTime = Geometry.timeUntilWallCollision(abs, ball.getShape(),
+				ball.getVolecity());
+		if (!isReflecting)
+			if (tempTime < GizmoSettings.getInstance()
+					.getBallMovementUpdateDtime()) {
+				isReflecting = true;
+				long msec = Utils.Sec2Msec(tempTime);
+				// update ball position on hit moment
+				GizmoDriver.getInstance().runTask(ball.newTask(tempTime), msec);
+				// run onHit method of gizmo on hit time
+				GizmoDriver.getInstance().runTask(new onColisionTimeTask(null),
+						msec);
+			}
+		return tempTime;
 	}
 
 	@Override
 	public void onColisionTime(GizmoBall ball, Object reflectigfrom) {
-		// TODO Auto-generated method stub
-		
+		isReflecting = false;
+		System.out.println("on colision");
+		GizmoBall b = new GizmoBall(300, (int)y, new Vect(50, -200));
+		GizmoBoard.getInstance().setBall(b);
+		b.startBallMovement();
+
 	}
 
 	@Override
@@ -87,16 +100,13 @@ public class Absorber extends AbstractGizmoModel {
 	@Override
 	public void onActivationEvent() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onDeactivationEvent() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-
-	
 }
